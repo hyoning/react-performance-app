6d4ba6ee2df7069d6ae65a32674dfc5976f280f2
김효은 <hyoeun@gmail.com>
2024-01-19 10:25:58 +0900

    react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import A from './components/A';
+import B from './components/B';
 
 function App() {
+
+  const [value, setValue] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(reponse => reponse.json()) //json 형식으로 데이터 받기
+    .then(posts => setPosts(posts)); // post 에 데이터가 들어가게 하기
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div style={{padding: '1rem'}}>
+      <input
+        value={value}
+        onChange = {e => setValue(e.target.value)}
+      />
+      <div style={{display:'flex'}}>
+          <A message={value} posts={posts}/>
+          <B message={value} posts={posts}/>
+      </div>
+     
+   </div>
   );
 }
 
